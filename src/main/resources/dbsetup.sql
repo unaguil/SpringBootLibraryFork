d515630746955fc83ef3e62fe0b37773f46679d2
dipina <dipina@deusto.es>
2025-04-14 08:55:44 +0200

    Added GitHub action to example

--- src/main/resources/dbsetup.sql
+++ src/main/resources/dbsetup.sql
@@ -1,10 +1,8 @@
+-- Script para crear la base de datos y el usuario para la API de biblioteca
 CREATE DATABASE IF NOT EXISTS libraryapidb;
 
 -- Crear usuario para conexiones remotas
-CREATE USER IF NOT EXISTS 'dbuser'@'%' IDENTIFIED BY 'dbuser';
-
--- Asegurar autenticación con mysql_native_password
-ALTER USER 'dbuser'@'%' IDENTIFIED WITH mysql_native_password BY 'dbuser';
+CREATE USER IF NOT EXISTS 'dbuser'@'%' IDENTIFIED WITH mysql_native_password BY 'dbuser';
 
 -- Conceder privilegios al usuario
 GRANT ALL PRIVILEGES ON libraryapidb.* TO 'dbuser'@'%' WITH GRANT OPTION;
